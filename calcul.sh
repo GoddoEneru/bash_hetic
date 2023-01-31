21ac3cf6ae0efb4cbbc06fb3ed26c6a2eaacaa40
Quentin <q.chalopin@gmail.com>
2023-01-31 18:18:15 +0100

    ajout calcul et carré

---
+++ calcul.sh
@@ -0,0 +1,3 @@
+read -p "rentrez votre calcul : " a op b
+let "result=$a $op $b"
+echo "la réponse est la suivante : $result"
---
+++ carre.sh
@@ -0,0 +1,3 @@
+read -p "rentrez un nombre : " n
+let "result=$n ** 2"
+echo "son carré est le suivant : $result"
