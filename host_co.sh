68effb04157e2ce3449edc942141ad0c3b0d7b06
Quentin <q.chalopin@gmail.com>
2023-02-01 15:16:50 +0100

    add offline txt host_co

--- host_co.sh
+++ host_co.sh
@@ -1,5 +1,7 @@
 for ((i=0;i<=255;i++)); do
     if ping 10.93.163.$i -c 1 -W 1
-    then echo 10.93.163.$i connected >> connected_hosts.txt
+    then echo 10.93.163.$i >> online.txt
+    else
+    echo 10.93.163.$i >> offline.txt
     fi
 done
